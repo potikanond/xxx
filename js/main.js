a411ab1890397320dd57014733902ae0033c41c4
dpotikan <dpotikan@eng.cmu.ac.th>
2019-12-17 16:30:58 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('This is my 1st JS Code')
+
+let str ='';
+for (let i=0; i<10; i++) {
+  console.log(i+1);
+  str += (i+1) + ' '
+}
+
+console.log(str);
+
+document.querySelector('#output').innerHTML = str;
